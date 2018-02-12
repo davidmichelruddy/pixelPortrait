d34add5e3085e7153196d1740e1ccaa1d3ee9f44
David Michel-Ruddy <davidmichelruddy@users.noreply.github.com>
2018-02-12 10:47:04 -0800

    changes some of the brush strokes, added buttons

--- particle.js
+++ particle.js
@@ -5,32 +5,36 @@ var Particle = function(x,y) {
 };
 
 Particle.prototype.update = function() {
-  this.x += random(-30,30);
-  this.y += random(-30,30);
-
+  this.x += random(-50,50);
+  this.y += random(-50,50);
   this.x = constrain(this.x,0, width);
   this.y = constrain(this.y,0, height);
 };
 
 Particle.prototype.show = function() {
-  if (frameCount > 20 && frameCount < 200) {
-    stroke(0,random(10,30));
+    strokeWeight(1);
+    stroke(0,random(20));
     var px = floor(this.x / vScale);
     var py = floor(this.y / vScale);
     var col = video.get(px,py);
-    fill(col[0], col[1], col[2], 100);
-    if (frameCount%2 === 0) {
-      ellipse(this.x,this.y,this.r,this.r);
-    } else if (frameCount%3 === 0) {
-      triangle(this.x,this.y, (this.x + this.r), (this.y - this.r), (this.x - this.r), (this.y - this.r));
-    } else if (frameCount%5 === 0) {
-      //strokeWeight(100);
-      line(this.x, this.y, this.x + random(50), this.y + random(50));
-    } else {
- rectMode(CENTER);
-      rect(this.x,this.y, this.r,this.r);
+    fill(col[0], col[1], col[2], random(100,200));
+    if (frameCount%7 === 0) {
+      ellipse(this.x,this.y,this.r + random(5),this.r + random(5));
     }
-  }
+    else if (frameCount%17 === 0) {
+      rotate(random(360));
+        triangle(this.x,this.y, (this.x + this.r), (this.y - this.r), (this.x - this.r), (this.y - this.r));
+      }
+    else {
+      for (var i = 0; i < 10; i++) {
+        stroke(col[0], col[1], col[2], random(100,150));
+        strokeWeight(random(10));
+        line(this.x,this.y,this.x+random(-10,10),this.y+random(-10,10));
+      }
+    }
+
+
+
 
     //ellipse(this.x,this.y,this.r,this.r);
 };
--- sketch.js
+++ sketch.js
@@ -2,49 +2,76 @@ var video;
 var vScale = 16;
 var particles = [];
 var triangles = [];
+var click = false;
+var count = 0;
+var c;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  c = createCanvas(windowWidth,windowHeight);
   pixelDensity(2);
+  background(0);
+  button = createButton('🎨 Paint My Portrait  ');
+  button.position(windowWidth/2, windowHeight/2);
+  button.mousePressed(clickToggle);
   video = createCapture(VIDEO);
   video.size(width/vScale, height/vScale);
   video.loadPixels();
-  //video.hide();
+  video.hide();
   for (var i = 0; i < 200; i++) {
     particles[i]  = new Particle(windowWidth/2,windowHeight/2);
   }
-
 //
-
-c1 = color(204, 102, 0);
-c2 = color(0, 102, 153);
-
-for (var j = 0; j < 20; j++) {
-  var tri = {
-    x1: (width/2) + random(200) -100,
-    y1: height/2 + random(200) -100,
-    x2: random(width * 2),
-    y2: random(height * 2),
-    x3: random(-width * 2),
-    y3: random(-height * 2),
-  };
-
-  triangles.push(tri);
-}
-
-triangles.forEach(function(shape){
-  fill(random(255));
-  //strokeWeight(random(50)/100); // range
-  noStroke();
-  triangle(shape.x1,shape.y1,shape.x2,shape.y2,shape.x3,shape.y3);
-});
 }
 
 function draw() {
   video.loadPixels();
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particles[i].show();
+  if (click && count < 1) {
+    triagleBackground();
   }
+
+  if (click && count <= 200) {
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].update();
+      particles[i].show();
+    }
+    count++;
+  }
+
+  if (count === 200) {
+    save = createButton('⬇️ Save My Portrait');
+    save.position(windowWidth/2, 3*windowHeight/4);
+    save.mousePressed(savePic);
+  }
+
   //filter(INVERT);
+}
+
+function triagleBackground() {
+  for (var j = 0; j < 20; j++) {
+    var tri = {
+      x1: (width/2) + random(200) -100,
+      y1: height/2 + random(200) -100,
+      x2: random(width * 2),
+      y2: random(height * 2),
+      x3: random(-width * 2),
+      y3: random(-height * 2),
+    };
+    triangles.push(tri);
+  }
+
+  triangles.forEach(function(shape){
+    fill(random(255));
+    //strokeWeight(random(50)/100); // range
+    noStroke();
+    triangle(shape.x1,shape.y1,shape.x2,shape.y2,shape.x3,shape.y3);
+  });
+}
+function clickToggle() {
+  click = true;
+  count = 0;
+  button.remove();
+}
+
+function savePic() {
+  saveCanvas(c, 'portraitOfYou_by_DMR', 'jpg');
 }
